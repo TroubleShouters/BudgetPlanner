3bcb0bfc7fc1bcb37c1a20a3f4330024ef305932
willyishmael <wtangka22@gmail.com>
2023-03-25 15:30:42 +0800

    main navigation

---
+++ app/src/main/java/com/troubleshouters/budgetplanner/ui/main/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.troubleshouters.budgetplanner.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.troubleshouters.budgetplanner.R
+import com.troubleshouters.budgetplanner.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navigationView: BottomNavigationView = binding.bottomNavigationView
+        val navigationController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_transactions_fragment,
+                R.id.navigation_plan_fragment,
+                R.id.navigation_settings_fragment
+            )
+        )
+
+        setupActionBarWithNavController(navigationController, appBarConfiguration)
+        navigationView.setupWithNavController(navigationController)
+
+    }
+}
