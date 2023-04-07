0f22b450436af79ef8e0d47419a2444c60b56974
willyishmael <wtangka22@gmail.com>
2023-04-07 18:18:33 +0800

    Create BudgetPlannerDatabase.kt

---
+++ app/src/main/java/com/troubleshouters/budgetplanner/data/local/BudgetPlannerDatabase.kt
@@ -0,0 +1,29 @@
+package com.troubleshouters.budgetplanner.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Plan::class, Transaction::class], version = 1)
+abstract class BudgetPlannerDatabase : RoomDatabase() {
+    abstract fun planDao(): PlanDao
+    abstract fun transactionDao(): TransactionDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: BudgetPlannerDatabase? = null
+
+        fun getInstance(context: Context): BudgetPlannerDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    BudgetPlannerDatabase::class.java,
+                    "budget_planner_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
