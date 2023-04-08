ee45cbd68df7e933e4dfec0234d9b27ddaebe85d
willyishmael <wtangka22@gmail.com>
2023-04-08 17:28:44 +0800

    add hilt annotation

--- app/src/main/java/com/troubleshouters/budgetplanner/ui/main/MainActivity.kt
+++ app/src/main/java/com/troubleshouters/budgetplanner/ui/main/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.troubleshouters.budgetplanner.R
 import com.troubleshouters.budgetplanner.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/com/troubleshouters/budgetplanner/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/troubleshouters/budgetplanner/ui/plan/PlanFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.troubleshouters.budgetplanner.databinding.FragmentPlanBinding
+import com.troubleshouters.budgetplanner.ui.viewmodel.PlanViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PlanFragment : Fragment() {
 
     private var _binding: FragmentPlanBinding? = null
     private val binding get() = _binding!!
+    private val planViewModel: PlanViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
