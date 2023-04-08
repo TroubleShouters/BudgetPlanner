1f91187af668ba000e175a40bc68a91836241f79
willyishmael <wtangka22@gmail.com>
2023-04-08 08:54:16 +0800

    Update PlanViewModel.kt

--- app/src/main/java/com/troubleshouters/budgetplanner/ui/viewmodel/PlanViewModel.kt
+++ app/src/main/java/com/troubleshouters/budgetplanner/ui/viewmodel/PlanViewModel.kt
@@ -1,13 +1,52 @@
 package com.troubleshouters.budgetplanner.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.troubleshouters.budgetplanner.data.local.plan.Plan
 import com.troubleshouters.budgetplanner.data.repository.PlanRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PlanViewModel @Inject constructor(
     private val planRepository: PlanRepository
 ) : ViewModel() {
+
+    private val _plan = MutableLiveData<Plan?>()
+    val plan: LiveData<Plan?> = _plan
+
+    val allPlans: LiveData<List<Plan>> = liveData {
+        val plans = planRepository.getAllPlans()
+        emit(plans)
+    }
+
+    fun getPlanById(planId: Long) {
+        viewModelScope.launch {
+            val plan = planRepository.getPlanById(planId)
+            _plan.value = plan
+        }
+    }
+
+    fun insertPlan(plan: Plan) {
+        viewModelScope.launch {
+            planRepository.insertPlan(plan)
+        }
+    }
+
+    fun updatePlan(plan: Plan) {
+        viewModelScope.launch {
+            planRepository.updatePlan(plan)
+        }
+    }
+
+    fun deletePlan(plan: Plan) {
+        viewModelScope.launch {
+            planRepository.deletePlan(plan)
+        }
+    }
 
 }
