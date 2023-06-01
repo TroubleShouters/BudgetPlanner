31de6350c6781672d5adf1d65357ca0b957cc849
willyishmael <wtangka22@gmail.com>
2023-06-01 15:24:32 +0800

    Update TransactionSummaryPagerAdapter.kt

--- app/src/main/kotlin/com/troubleshouters/budgetplanner/ui/adapter/TransactionSummaryPagerAdapter.kt
+++ app/src/main/kotlin/com/troubleshouters/budgetplanner/ui/adapter/TransactionSummaryPagerAdapter.kt
@@ -1,31 +1,47 @@
 package com.troubleshouters.budgetplanner.ui.adapter
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.troubleshouters.budgetplanner.data.local.transaction.Transaction
+import com.troubleshouters.budgetplanner.data.local.plan.Plan
+import com.troubleshouters.budgetplanner.databinding.ViewTransactionSummaryBinding
 
 class TransactionSummaryPagerAdapter(
-    private val transactions: List<Transaction>
+    private val plans: List<Plan>
 ) : RecyclerView.Adapter<TransactionSummaryPagerAdapter.TransactionSummaryViewHolder>() {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): TransactionSummaryViewHolder {
-        TODO("Not yet implemented")
+        val binding = ViewTransactionSummaryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TransactionSummaryViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        return transactions.count()
+        return plans.count()
     }
 
-    override fun onBindViewHolder(holder: TransactionSummaryViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(
+        holder: TransactionSummaryViewHolder,
+        position: Int
+    ) {
+        holder.bind(plans[position])
     }
 
-    inner class TransactionSummaryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class TransactionSummaryViewHolder(
+        private val binding: ViewTransactionSummaryBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(plan: Plan) {
+            binding.apply {
+                tvPlanType.text = plan.planType.toString()
+                // tvDate.text
+                tvPlanTitle.text = plan.title
+                // tvPlanAmount.text
+
+            }
+        }
     }
 
 }
